681c03d9db8de31bebf578154b86a2eacce7c0cf
Vaibhav <vaibhavnath96@gmail.com>
2025-07-18 17:55:17 +0200

    added tsp model

---
+++ src/tsp.py
@@ -0,0 +1,293 @@
+import json
+import time
+
+import gamspy as gp
+from gamspy.exceptions import ValidationError, GamspyException
+
+import networkx as nx
+import numpy as np
+import pandas as pd
+
+
+def mtz_formulation(m: gp.Container) -> gp.Equation:
+    n1, n2, i, j, ij, X, start_point = m.getSymbols(
+        ["n1", "n2", "i", "j", "allowed_arcs", "X", "start_point"]
+    )
+
+    P = gp.Variable(m, "p", type="positive", domain=n1)
+
+    P.fx[start_point] = 0
+    P.lo[n1].where[~n1.sameAs(start_point)] = 1
+    P.up[n1] = gp.Card(n2) - 1
+
+    eq_mtz = gp.Equation(
+        m,
+        "eq_mtz",
+        description="Miller, Tucker and Zemlin subtour elimination",
+        domain=[n1, n2],
+    )
+
+    eq_mtz[i, j].where[ij[i, j] & ~i.sameAs(start_point) & ~j.sameAs(start_point)] = P[
+        i
+    ] - P[j] + 1 <= gp.Card(i) * (1 - X[i, j])
+
+    return [eq_mtz]
+
+
+def explicit_dfj_formulation(m: gp.Container) -> gp.Equation:
+    n1, i, j, ij, X = m.getSymbols(["n1", "i", "j", "allowed_arcs", "X"])
+
+    s = gp.Set(
+        m,
+        "s",
+        domain="*",
+        description="Powerset",
+    )
+    sn = gp.Set(m, "sn", domain=[s, n1], description="subset_membership")
+
+    subset_size = gp.Parameter(
+        m,
+        "subset_size",
+        domain=[s],
+        description="Number of elements in each subset",
+    )
+
+    filtered_set = gp.Set(
+        m,
+        "filtered_powetset",
+        domain=[s],
+        description="Subsets excluding empty, singletons, and full set",
+    )
+
+    s.setRecords([f"s{subset}" for subset in range(2 ** len(i))])
+    sn[s, n1].where[
+        i[n1]
+        & gp.math.mod(
+            gp.math.floor((gp.Ord(s) - 1) / gp.math.power(2, gp.Ord(n1) - 1)), 2
+        )
+        == 1
+    ] = True
+
+    subset_size[s] = gp.Sum(i.where[sn[s, i]], gp.Number(1))
+    filtered_set[s].where[(subset_size[s] > 1) & (subset_size[s] < len(i))] = True
+
+    eq_dfj = gp.Equation(
+        m,
+        "eq_dfj",
+        domain=[s],
+        description="Explicit Dantzig-Fulkerson-Johnson subtour elimination constraint.",
+    )
+    eq_dfj[filtered_set[s]] = (
+        gp.Sum(gp.Domain(i, j).where[(ij[i, j]) & (sn[s, i]) & (sn[s, j])], X[i, j])
+        <= subset_size[s] - 1
+    )
+
+    return [eq_dfj]
+
+
+def find_illegal_subtour(sol: pd.DataFrame, start_point):
+    G = nx.DiGraph()
+    G.add_edges_from(
+        [(i, j) for i, j in sol[["n1", "n2"]].itertuples(index=False, name=None)]
+    )
+    components = list(nx.strongly_connected_components(G))
+
+    return [list(comp) for comp in components if start_point not in comp]
+
+
+def tspModel(
+    nodes_recs, distance_recs, **options
+) -> tuple[list[pd.DataFrame, float], gp.Model]:
+    solver: str = options["solver"]
+    time_limit: int = options["time_limit"]
+    maxnodes: int = options["maxnodes"]
+    formulation: str = options["formulation"]
+
+    m = gp.Container()
+
+    nodes = gp.Set(m, name="set_of_nodes", domain="*")
+
+    start_point = gp.Set(
+        m,
+        "start_point",
+        domain=nodes,
+        is_singleton=True,
+    )
+
+    n1 = gp.Alias(m, name="n1", alias_with=nodes)
+    n2 = gp.Alias(m, name="n2", alias_with=nodes)
+
+    i = gp.Set(m, name="i", domain=nodes, description="subset of nodes")
+
+    j = gp.Alias(m, name="j", alias_with=i)
+
+    ij = gp.Set(m, name="allowed_arcs", domain=[n1, n2])
+
+    distance = gp.Parameter(m, name="distance_matrix", domain=[n1, n2])
+
+    total_cost = gp.Variable(m, name="total_cost")
+
+    X = gp.Variable(
+        m,
+        name="x",
+        type="binary",
+        domain=[n1, n2],
+        description="decision variable - leg of trip",
+    )
+
+    objective_function = gp.Equation(m, name="tsp_objective_function")
+    objective_function[...] = gp.Sum(ij[i, j], distance[i, j] * X[i, j]) == total_cost
+
+    eq_enter_once = gp.Equation(m, "eq_enter_once", domain=[n1])
+    eq_enter_once[j].where[~j.sameAs(start_point)] = (
+        gp.Sum(i.where[ij[i, j]], X[i, j]) == 1
+    )
+
+    eq_leave_once = gp.Equation(m, "eq_leave_once", domain=[n1])
+    eq_leave_once[i].where[~i.sameAs(start_point)] = (
+        gp.Sum(j.where[ij[i, j]], X[i, j]) == 1
+    )
+
+    nodes.setRecords(nodes_recs["row.city"])
+    start_point.setRecords([nodes.records.iloc[0]["uni"]])
+    distance.setRecords(distance_recs)
+
+    i[n1].where[gp.Ord(n1) <= maxnodes] = True
+    ij[i, j].where[~i.sameAs(j)] = True
+
+    if formulation.upper() == "IDFJ":
+        s = gp.Set(
+            m,
+            "s",
+            domain="*",
+            description="Powerset",
+            records=range(1000),
+        )
+
+        active_cut = gp.Set(m, "active_cut", domain=[s])
+        sn = gp.Set(m, "sn", domain=[s, n1], description="subset_membership")
+
+        eq_dfj_iter = gp.Equation(m, "eq_dfj_iter", domain=[s])
+
+        eq_dfj_iter[active_cut] = (
+            gp.Sum(
+                gp.Domain(i, j).where[
+                    (ij[i, j]) & (sn[active_cut, i]) & (sn[active_cut, j])
+                ],
+                X[i, j],
+            )
+            <= gp.Sum(i.where[sn[active_cut, i]], gp.Number(1)) - 1
+        )
+
+        tsp = gp.Model(
+            m,
+            name="tsp",
+            problem="MIP",
+            sense=gp.Sense.MIN,
+            objective=total_cost,
+            equations=[objective_function, eq_enter_once, eq_leave_once, eq_dfj_iter],
+        )
+
+        tsp.solve(solver=solver, options=gp.Options(time_limit=time_limit))
+
+        cnt = 0
+        MAXCUTS = len(s)
+        tot_time = 0
+        set_of_tour = gp.Set(m, "set_of_tour", domain=[n1])
+
+        while True:
+            start = time.time()
+            sol = X[...].where[X.l > 0.5].records
+            subtours = find_illegal_subtour(
+                sol, start_point=start_point.records["set_of_nodes"][0]
+            )
+
+            if not subtours:
+                print("***All illegal subtours are removed. Soution found!***")
+                break
+
+            if cnt + len(subtours) > MAXCUTS:
+                raise GamspyException(
+                    f"Found {len(subtours)} illegal subtours, but adding them would"
+                    f" exceed the cut limit of {MAXCUTS}."
+                )
+
+            for idx, tour in enumerate(subtours, start=cnt):
+                set_of_tour.setRecords(tour)
+                sn[idx, set_of_tour] = True
+
+            cnt += len(subtours)
+            print(
+                f"illegal subtours in current solution: {len(subtours)} | total subtours: {cnt}"
+            )
+            active_cut[s] = gp.Ord(s) <= cnt
+            tsp.solve(solver=solver, options=gp.Options(time_limit=time_limit))
+            tot_time += time.time() - start
+
+            if tot_time > time_limit:
+                print("Total timelimit reached. Stopping!")
+                break
+
+        return [sol, tot_time], tsp
+
+    elif formulation.upper() == "MTZ":
+        subtour_eliminate_equation = mtz_formulation(m)
+    elif formulation.upper() == "EDFJ":
+        subtour_eliminate_equation = explicit_dfj_formulation(m)
+    else:
+        raise ValidationError(
+            f"Wrong choice of formulation >{formulation}<. Choose from ['MTZ', 'EDFJ', 'IDFJ']"
+        )
+
+    tsp = gp.Model(
+        m,
+        name="tsp",
+        problem="MIP",
+        sense=gp.Sense.MIN,
+        objective=total_cost,
+        equations=[objective_function, eq_enter_once, eq_leave_once]
+        + subtour_eliminate_equation,
+    )
+
+    tsp.solve(solver=solver, options=gp.Options(time_limit=time_limit))
+    sol = X[...].where[X.l > 0.5].records
+
+    return [sol, 0], tsp
+
+
+def main():
+    maxnodes = 10
+    time_limit = 30
+    solver = "CPLEX"
+    formulation = "MTZ"
+    options = {
+        "solver": solver,
+        "time_limit": time_limit,
+        "maxnodes": maxnodes,
+        "formulation": formulation,
+    }
+
+    def euclidean_distance_matrix(coords):
+        diff = coords[:, np.newaxis, :] - coords[np.newaxis, :, :]
+        dist_matrix = np.sqrt(np.sum(diff**2, axis=-1))
+        return dist_matrix
+
+    with open(r"..\germany_cities.json", "r") as fp:
+        city_data = json.load(fp)
+
+    city_df = pd.json_normalize(city_data)
+    dist_mat = euclidean_distance_matrix(
+        city_df[["row.latitude", "row.longitude"]].to_numpy()
+    )
+    dist_df = pd.DataFrame(
+        dist_mat, index=city_df["row.city"], columns=city_df["row.city"]
+    )
+    distance_df = dist_df.reset_index().melt(
+        id_vars="row.city", var_name="to_city", value_name="distance"
+    )
+
+    tspModel(nodes_recs=city_df, distance_recs=distance_df, **options)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tsp-app.py
@@ -0,0 +1,341 @@
+import json
+import requests
+
+import numpy as np
+import pandas as pd
+
+import streamlit as st
+import folium
+from streamlit_folium import st_folium
+from folium.plugins import PolyLineTextPath
+from folium.plugins import AntPath
+
+from src.tsp import tspModel
+
+
+st.set_page_config(layout="wide")
+
+with open("available_countries.json", "r") as fp:
+    LIST_OF_COUNTRIES = json.load(fp)
+
+SUPPORTED_FORMULATIONS = {
+    "Miller-Tucker-Zemlin": "MTZ",
+    "Explicit-DFJ": "EDFJ",
+    "Iterative-DFJ": "IDFJ",
+}
+
+
+@st.dialog(title="Info")
+def formulation_info(selected):
+    if selected == "Miller-Tucker-Zemlin":
+        st.write(
+            "The MTZ *(Miller-Tucker-Zemlin)* formulation eliminates subtours by introducing auxiliary variables that represent the order in which each city is visited in the tour. These variables enable linear constraints to enforce a valid tour without subtours."
+        )
+    elif selected == "Explicit-DFJ":
+        st.write(
+            "The Explicit-DFJ *(Dantzig-Fulkerson-Johnson)* formulation for the TSP adds a subtour elimination constraint for every nontrivial subset of cities—that is, it considers the entire powerset of the city set (except the empty and full sets) to prevent subtours. This approach becomes computationally infeasible when the number of cities exceeds about 10, due to the exponential growth in the number of subsets."
+        )
+    elif selected == "Iterative-DFJ":
+        st.write(
+            "The Iterative-DFJ *(Dantzig-Fulkerson-Johnson)* method repeatedly solves the TSP model, adding subtour elimination constraints whenever subtours are found in the current solution, and continues this process until the final solution forms a valid tour visiting all cities exactly once."
+        )
+
+
+DEFAULTS = {
+    "solutionFound": False,
+    "obj_val": 0,
+    "path": [],
+    "stats": {},
+}
+
+for key, val in DEFAULTS.items():
+    if key not in st.session_state:
+        st.session_state[key] = val
+
+
+def reset_to_default_view():
+    """Resets the session state to its default values."""
+    for key, val in DEFAULTS.items():
+        st.session_state[key] = val
+    st.info("Inputs changed. Click 'Run Optimization' to generate a new solution.")
+
+
+@st.cache_data(show_spinner=False)
+def fetchCities(country):
+    base_url = "https://datasets-server.huggingface.co/filter"
+    params = {
+        "dataset": "jamescalam/world-cities-geo",
+        "config": "default",
+        "split": "train",
+        "where": f"country='{country}'",
+        "length": 100,
+    }
+
+    response = requests.get(base_url, params=params)
+    if response.status_code != 200:
+        st.error("Something went wrong while fetching the cities.")
+
+    url = f"https://services9.arcgis.com/l9yXFvhjz46ekkZV/arcgis/rest/services/Countries_Centroids/FeatureServer/0/query?where=COUNTRY+%3D+%27{country}%27&outFields=*&f=pgeojson"
+
+    country_center = requests.get(url)
+    if country_center.status_code != 200:
+        st.error(
+            "Something went wrong while fetching coordinates for the selected country."
+        )
+
+    country_centroid = country_center.json()["features"][0]["properties"]
+
+    return response.json()["rows"], [
+        country_centroid["latitude"],
+        country_centroid["longitude"],
+    ]
+
+
+def solve_tsp(city_df, distance_df, **config):
+    obj_val = 0
+    path = []
+    stats = pd.DataFrame(columns=["Value"])
+
+    try:
+        sol_list, model = tspModel(city_df, distance_df, **config)
+        sol, time_taken = sol_list
+    except Exception as e:
+        raise Exception(e)
+
+    if model.solve_status.value not in [1, 2, 3, 5, 8]:
+        st.error(
+            f"Solution not found. Solver returned with status: {model.solve_status}"
+        )
+        st.session_state["solutionFound"] = False
+    else:
+        st.session_state["solutionFound"] = True
+        stats_data = {
+            "statistics": {
+                "Objective Value": model.objective_value,
+                "Solve Time (s)": model.solve_model_time
+                if time_taken == 0
+                else time_taken,
+                "Status": model.status.name,
+                "#Variables": model.num_variables,
+                "#Constraints": model.num_equations,
+            }
+        }
+        stats = pd.DataFrame.from_dict(
+            stats_data["statistics"], orient="index", columns=["Value"]
+        )
+        stats["Value"] = stats["Value"].astype(str)
+
+        path = [start := sol.n1.iloc[0]]
+        while (next := sol.loc[sol.n1 == path[-1], "n2"].values[0]) != start:
+            path.append(next)
+        path.append(start)
+        obj_val = model.objective_value
+
+    st.session_state["obj_val"] = obj_val
+    st.session_state["path"] = path
+    st.session_state["stats"] = stats
+
+
+def prepInput():
+    country_centroid = [51.1657, 10.4515]  # DE
+    city_df = pd.DataFrame(columns=["row.city", "row.latitude", "row.longitude"])
+
+    with st.sidebar:
+        selected_country = st.selectbox(
+            "Choose a country",
+            LIST_OF_COUNTRIES,
+            index=None,
+            placeholder="Type or select a country..",
+            help="This tries to fetch 100 cities with their geolocation data from a Kaggle dataset",
+            on_change=reset_to_default_view,
+        )
+        st.write("")
+        st.write("")
+        st.write("")
+        select_formulation = st.radio(
+            "Choose a formulation for TSP:",
+            SUPPORTED_FORMULATIONS.keys(),
+            index=0,
+            help="Select the type of formulation used to eliminate subtours.",
+            on_change=reset_to_default_view,
+        )
+
+        if st.button("Info", type="secondary"):
+            formulation_info(select_formulation)
+
+        st.divider()
+
+        solver = st.text_input("Solver", value="CPLEX", on_change=reset_to_default_view)
+        number_of_nodes = st.number_input(
+            "Maximum nodes to consider for TSP",
+            min_value=1,
+            max_value=100,
+            value=10,
+            on_change=reset_to_default_view,
+        )
+        seed = st.number_input(
+            "Random Seed for selecting the starting node",
+            min_value=1,
+            max_value=100,
+            value=49,
+            on_change=reset_to_default_view,
+        )
+        timeLimit = st.number_input(
+            "Time limit (seconds)",
+            min_value=1,
+            max_value=60,
+            value=30,
+            on_change=reset_to_default_view,
+        )
+
+        if selected_country:
+            with st.spinner("Fetching Cities..."):
+                city_data, country_centroid = fetchCities(selected_country)
+                city_df = pd.json_normalize(city_data)
+                city_df = city_df[["row.city", "row.latitude", "row.longitude"]]
+                try:
+                    city_df = city_df.sample(
+                        number_of_nodes, random_state=seed
+                    ).reset_index(drop=True)
+                except ValueError:
+                    raise Exception(
+                        "Maxnimum nodes is greater than number of cities available. Reduce maximum number of nodes."
+                    )
+
+    input_config = {
+        "time_limit": timeLimit,
+        "solver": solver,
+        "maxnodes": number_of_nodes,
+        "formulation": SUPPORTED_FORMULATIONS[select_formulation],
+        "centroid": country_centroid,
+        "country": selected_country,
+    }
+
+    return city_df, input_config
+
+
+@st.cache_data(show_spinner=False)
+def euclidean_distance_matrix(coords):
+    diff = coords[:, np.newaxis, :] - coords[np.newaxis, :, :]
+    dist_matrix = np.sqrt(np.sum(diff**2, axis=-1))
+    return dist_matrix
+
+
+@st.cache_data
+def plot_solution(city_df: pd.DataFrame, centroid: list[float], path: list):
+    country_map = folium.Map(location=centroid, zoom_start=5)
+
+    if len(city_df):
+        for idx, (name, lat, long) in enumerate(
+            city_df.itertuples(index=False, name=None)
+        ):
+            if idx == 0:
+                folium.Marker(
+                    [lat, long],
+                    popup=name,
+                    icon=folium.Icon(color="green", prefix="fa", icon="location-dot"),
+                ).add_to(country_map)
+            else:
+                # Standard marker for others
+                folium.Marker(
+                    [lat, long], popup=name, icon=folium.Icon(icon="info-sign")
+                ).add_to(country_map)
+
+    if len(path):
+        path_df = pd.DataFrame({"row.city": path})
+        final_path = path_df.merge(city_df, on="row.city", how="left")
+
+        AntPath(
+            locations=final_path[["row.latitude", "row.longitude"]],
+            color="#FF0000",
+            delay=1000,
+            dash_array=[20, 30],
+            weight=5,
+            opacity=0.8,
+        ).add_to(country_map)
+
+        final_path = pd.concat([final_path, final_path.iloc[[0]]], ignore_index=True)
+
+        polyline = folium.PolyLine(
+            final_path[["row.latitude", "row.longitude"]],
+        ).add_to(country_map)
+
+        arrows = PolyLineTextPath(
+            polyline,
+            "➔",
+            repeat=True,
+            offset=7,
+            attributes={"fill": "red", "font-weight": "light", "font-size": "20"},
+        )
+        arrows.add_to(country_map)
+
+    return country_map
+
+
+def main():
+    st.title("GAMSPy TSP Solver")
+
+    city_df, config = prepInput()
+
+    with st.sidebar:
+        st.divider()
+        st.write("")
+        st.write("")
+        run_opt = st.button("Run Optimization", type="primary")
+
+    maxnodes = config["maxnodes"]
+    formulation = config["formulation"]
+    country_centroid = config["centroid"]
+
+    if formulation == "EDFJ" and maxnodes > 10:
+        st.toast(
+            f"Explicit-DFJ would generate {2**maxnodes} subsets which might not work with a Demo License.",
+            icon="⚠️",
+        )
+
+    col1, col2 = st.columns(2)
+
+    with col2:
+        output_placeholder = st.empty()
+        if run_opt:
+            dist_mat = euclidean_distance_matrix(
+                city_df[["row.latitude", "row.longitude"]].to_numpy()
+            )
+            dist_df = pd.DataFrame(
+                dist_mat, index=city_df["row.city"], columns=city_df["row.city"]
+            )
+            distance_df = dist_df.reset_index().melt(
+                id_vars="row.city", var_name="to_city", value_name="distance"
+            )
+
+            with st.spinner("Solving TSP..."):
+                output_placeholder.empty()
+                if city_df.empty:
+                    raise st.error("Select a country first")
+                print(f"Solving TSP-{formulation} for {config["country"]}...")
+                solve_tsp(city_df, distance_df, **config)
+
+
+        if st.session_state["solutionFound"]:
+            with output_placeholder.container():
+                st.success("Solution Found!")
+                st.markdown("### Optimal Tour:")
+                st.write(" ➡️ ".join(st.session_state["path"]))
+                st.markdown("### Total distance travelled:")
+                st.markdown(
+                    f"{st.session_state['obj_val'] * 100: .2f} kms",
+                )
+                st.markdown("### Model Statistics:")
+                st.dataframe(st.session_state["stats"])
+        else:
+            output_placeholder.empty()
+
+    with col1:
+        path = st.session_state.get("path", [])
+        country_map = plot_solution(city_df, country_centroid, path=path)
+        with st.spinner("Plotting solution path..."):
+            st_folium(country_map, use_container_width=True, height=700)
+
+
+if __name__ == "__main__":
+    main()
